878a70545029fd294cda63f041c660f6dec5366f
yangtau <yanggtau+github@gmail.com>
2020-12-10 21:06:57 +0800

    feat: result

--- src/include/result.h
+++ src/include/result.h
@@ -1,8 +1,56 @@
 #pragma once
 
+#include <functional>
+#include <utility>
+#include <variant>
+
 namespace bolo {
 
 template <typename T, typename E>
-class Result {};
+class Result {
+ public:
+  static auto Ok(const T& t) { return Result(t); }
+  static auto Ok(T&& t) { return Result(std::move<T>(t)); }
+  static auto Err(const E& e) { return Result(e); }
+  static auto Err(E&& e) { return Result(std::move<E>(e)); }
+  explicit operator bool() const { return std::holds_alternative<T>(result_); }
+  T value() const { return std::get<T>(result_); }
+  T value_or(T default_value) const { return *this ? std::get<T>(result_) : default_value; }
+  E error() const { return std::get<E>(result_); }
 
+  template <typename Function>
+  auto Map(Function fn) const -> Result<decltype(fn(T())), E> {
+    if (*this) {
+      return Result(fn(value()));
+    } else {
+      return Result(error());
+    }
+  }
+
+  template <typename Function>
+  auto MapOr(Function fn, decltype(fn(T())) default_value) const {
+    using U = decltype(fn(T()));
+    if (*this) {
+      return Result(fn(value()));
+    } else {
+      return Result<U, E>(default_value);
+    }
+  }
+
+  template <typename Function>
+  auto operator>>=(Function fn) const -> decltype(Map(fn)) {
+    return Map(fn);
+  }
+
+ private:
+  explicit Result(T t) : result_{t} {
+    static_assert(!std::is_same<T, E>::value, "Result: is_same<T, E> == true");
+  }
+
+  explicit Result(E e) : result_{e} {
+    static_assert(!std::is_same<T, E>::value, "Result: is_same<T, E> == true");
+  }
+
+  const std::variant<T, E> result_;
+};
 };  // namespace bolo
---
+++ src/test/include_test/test.cc
@@ -0,0 +1,46 @@
+#define CATCH_CONFIG_MAIN
+
+#include <result.h>
+
+#include <catch.hpp>
+#include <string>
+
+using namespace bolo;
+
+TEST_CASE("Result", "test") {
+  int v{20};
+  auto err_msg = std::string{"Unexpected Error"};
+  auto square = [](int a) { return a * a; };
+  auto div3 = [](int a) -> double { return a / 3.0; };
+
+  auto r1 = Result<int, std::string>::Ok(v);
+  auto r2 = Result<int, std::string>::Err(err_msg);
+
+  SECTION("bool") {
+    REQUIRE(static_cast<bool>(r1));
+
+    REQUIRE(static_cast<bool>(r2) == false);
+    REQUIRE(!r2);
+  }
+
+  SECTION("value") { REQUIRE(r1.value() == v); }
+  SECTION("value_or") { REQUIRE(r2.value_or(v) == v); }
+  SECTION("error") { REQUIRE(r2.error() == err_msg); }
+  SECTION("map") {
+    auto r = r1.Map(square) >>= square;
+    REQUIRE(r.value() == v * v * v * v);
+
+    auto r3 = (r2 >>= square);
+
+    REQUIRE(!r3);
+    REQUIRE(r3.error() == err_msg);
+    REQUIRE(r3.value_or(v) == v);
+  }
+
+  SECTION("map_or") {
+    auto r = r2.MapOr(square, v);
+    REQUIRE(static_cast<bool>(r));
+
+    REQUIRE(r2.Map(square).MapOr(square, v).value() == v);
+  }
+}
