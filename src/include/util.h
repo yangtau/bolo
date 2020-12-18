411dca719915da3977fd720ae8467eca96692284
yangtau <yanggtau+github@gmail.com>
2020-12-18 21:12:52 +0800

    feat: timestamp to datetime str

--- src/include/util.h
+++ src/include/util.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+#include <string>
 
 #include "types.h"
 
@@ -14,7 +18,21 @@
 namespace bolo {
 inline Timestamp GetTimestamp() {
   using namespace std::chrono;
-  auto now = high_resolution_clock::now();
-  return static_cast<Timestamp>(duration_cast<nanoseconds>(now.time_since_epoch()).count());
+  auto now = system_clock::now();
+  return static_cast<Timestamp>(duration_cast<microseconds>(now.time_since_epoch()).count());
 }
+
+inline std::string TimestampToString(Timestamp stamp) {
+  namespace ch = std::chrono;
+
+  ch::microseconds s{static_cast<ch::microseconds>(stamp)};
+  ch::system_clock::time_point p{ch::duration{s}};
+
+  auto t = ch::system_clock::to_time_t(p);
+
+  std::stringstream ss;
+  ss << std::put_time(std::localtime(&t), "%F %T");
+  return ss.str();
+}
+
 };  // namespace bolo
