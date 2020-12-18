ba8dfb1b2b47777c699954d63a56fa355f4b9505
yangtau <yanggtau+github@gmail.com>
2020-12-18 14:39:37 +0800

    feat: improve Result

--- src/include/result.h
+++ src/include/result.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <functional>
 #include <type_traits>
 #include <variant>
 
@@ -10,14 +9,16 @@ namespace bolo {
 template <typename T, typename U>
 class Either : private std::variant<T, U> {
  public:
-  Either(const T &t) : std::variant<T, U>(t) {}
-  Either(T &&t) : std::variant<T, U>(std::move(t)) {}
-  Either(const U &u) : std::variant<T, U>(u) {}
-  Either(U &&u) : std::variant<T, U>(std::move(u)) {}
+  Either(const T &t) : std::variant<T, U>{t} {}
+  Either(T &&t) : std::variant<T, U>{std::move(t)} {}
+  Either(const U &u) : std::variant<T, U>{u} {}
+  Either(U &&u) : std::variant<T, U>{std::move(u)} {}
 
   bool HoldsLeft() const { return std::holds_alternative<T>(*this); }
   bool HoldsRight() const { return std::holds_alternative<U>(*this); }
 
+  T &left() { return std::get<T>(*this); }
+  U &right() { return std::get<U>(*this); }
   const T &left() const { return std::get<T>(*this); }
   const U &right() const { return std::get<U>(*this); }
 };
@@ -33,9 +34,9 @@ struct Ok {
 // Err
 template <typename E>
 struct Err {
-  E value;
-  explicit Err(const E &e) : value{e} {}
-  explicit Err(E &&e) : value{std::move(e)} {}
+  E error;
+  explicit Err(const E &e) : error{e} {}
+  explicit Err(E &&e) : error{std::move(e)} {}
 };
 
 // Result
@@ -44,108 +45,98 @@ class Result : private Either<Ok<T>, Err<E>> {
   using Ei = Either<Ok<T>, Err<E>>;
 
  public:
-  Result(const Ok<T> &ok) : Ei(ok) {}
-  Result(Ok<T> &&ok) : Ei(std::move(ok)) {}
-  Result(const Err<E> &err) : Ei(err) {}
-  Result(Err<E> &&err) : Ei(std::move(err)) {}
+  Result(const Ok<T> &ok) : Ei{ok} {}
+  Result(Ok<T> &&ok) : Ei{std::move(ok)} {}
+  Result(const Err<E> &err) : Ei{err} {}
+  Result(Err<E> &&err) : Ei{std::move(err)} {}
 
   explicit operator bool() const { return this->HoldsLeft(); }
+
   bool operator==(const Result<T, E> &r) const {
     if (r && *this) return r.value() == this->value();
-    if (!r && !*this) return r.error() == this->error();  // Nothing
+    if (!r && !*this) return r.error() == this->error();
     return false;
   }
 
+  T &value() { return this->left().value; }
   const T &value() const { return this->left().value; }
+
+  T &value_or(T &default_value) { return *this ? value() : default_value; }
   const T &value_or(const T &default_value) const { return *this ? value() : default_value; }
 
-  E error() const { return this->right().value; }
+  // T should be copyable
+  T value_or(T &&default_value) const { return *this ? value() : default_value; }
 
-  // Function: T -> U
-  // Map: Function -> Result<U, E>
-  template <typename Function, typename U = typename std::result_of<Function(T)>::type>
-  auto Map(Function fn) const -> Result<U, E> {
-    if (this->HoldsLeft()) return Ok(fn((value())));
-    return Err(error());
-  }
-
-  // Function: T -> U
-  // Map: Function -> U -> Result<U, E>
-  template <typename Function, typename U = typename std::result_of<Function(T)>::type>
-  auto MapOr(Function fn, const U &default_value) const -> Result<U, E> {
-    if (this->HoldsLeft()) return Ok(fn((value())));
-    return Ok(default_value);
-  }
-
-  // Function: T -> Result<U, E>
-  // >>=: Function -> Result<U, E>
-  template <typename Function, typename R = typename std::result_of<Function(T)>::type>
-  auto operator>>=(Function fn) const -> R {
-    if (*this) return fn((value()));
-    return Err(error());
-  }
+  E &error() { return this->right().error; }
+  const E &error() const { return this->right().error; }
 
   // Returns res if the result is Err, otherwise returns this
-  Result<T, E> operator|(const Result<T, E> &res) const { return *this ? *this : res; }
+  Result<T, E> &Or(Result<T, E> &res) { return *this ? *this : res; }
+  // T should be copyable
+  Result<T, E> Or(Result<T, E> &&res) const { return *this ? *this : res; }
 
   // Returns res if the result is Ok, otherwise return this
-  Result<T, E> operator&(const Result<T, E> &res) const { return *this ? res : *this; }
+  Result<T, E> &And(Result<T, E> &res) { return *this ? res : *this; }
+  // T should be copyable
+  Result<T, E> And(Result<T, E> &&res) const { return *this ? res : *this; }
 };
 
-// Nothing
-namespace details {
-struct nothing_t {};
-};  // namespace details
+// |: Result<T, E> -> Function (T -> U) -> Result<U, E>
+template <typename Function, typename T, typename E,
+          typename = std::enable_if<std::is_invocable_v<Function, T &>>,
+          typename U = typename std::invoke_result_t<Function, T>>
+Result<U, E> operator|(Result<T, E> &res, Function fn) {
+  if (res) return Ok(fn(res.value()));
+  return Err(res.error());
+}
+template <typename Function, typename T, typename E,
+          typename = std::enable_if<std::is_invocable_v<Function, T &>>,
+          typename U = typename std::invoke_result_t<Function, T>>
+Result<U, E> operator|(Result<T, E> &&res, Function fn) {
+  if (res) return Ok(fn(std::move(res.value())));
+  return Err(res.error());
+}
 
-constexpr details::nothing_t Nothing;
+// >>: Result<T, E> -> Function (T -> Result<U, E>) -> Result<U, E>
+template <typename Function, typename T, typename E,
+          typename = std::enable_if<std::is_invocable_v<Function, T &>>,
+          typename R = typename std::invoke_result_t<Function, T>>
+R operator>>(Result<T, E> &res, Function fn) {
+  if (res) return fn(res.value());
+  return Err(res.error());
+}
+
+template <typename Function, typename T, typename E,
+          typename = std::enable_if<std::is_invocable_v<Function, T &>>,
+          typename R = typename std::invoke_result_t<Function, T>>
+R operator>>(Result<T, E> &&res, Function fn) {
+  if (res) return fn(std::move(res.value()));
+  return Err(res.error());
+}
+
+// Nothing
+
+namespace details {
+class NothingType {};
+};  // namespace details
+constexpr details::NothingType Nothing{};
 
 // Maybe
 template <typename T>
-class Maybe : private Either<T, details::nothing_t> {
-  using E = Either<T, details::nothing_t>;
+class Maybe : public Result<T, details::NothingType> {
+ private:
+  using R = Result<T, details::NothingType>;
 
  public:
-  explicit Maybe(const T &t) : E(t) {}
-  explicit Maybe(T &&t) : E(std::move(t)) {}
-  Maybe(const details::nothing_t &) : E(Nothing) {}
+  explicit Maybe(const T &t) : R{Ok{t}} {}
+  explicit Maybe(T &&t) : R{Ok{(std::move(t))}} {}
+  Maybe(details::NothingType) : R{Err<details::NothingType>{Nothing}} {}
+  Maybe(Err<details::NothingType>) : R{Err<details::NothingType>{Nothing}} {}
 
-  explicit operator bool() const { return this->HoldsLeft(); }
   bool operator==(const Maybe<T> &m) const {
     if (m && *this) return m.value() == this->value();
-    if (!m && !*this) return true;  // Nothing
-    return false;
+    return !m && !*this;
   }
-
-  const T &value() const { return this->left(); }
-  const T &value_or(const T &default_value) const { return *this ? value() : default_value; }
-
-  // Map: Function(T->U) -> Maybe<U>
-  template <typename Function, typename U = typename std::result_of<Function(T)>::type>
-  auto Map(Function fn) -> Maybe<U> {
-    if (this->HoldsLeft()) return Maybe<U>(fn(value()));
-    return *this;  // Nothing
-  }
-
-  // Function: T -> U
-  // MapOr: Function -> U -> Maybe<U>
-  template <typename Function, typename U = typename std::result_of<Function(T)>::type>
-  auto MapOr(Function fn, const U &u) -> Maybe<U> {
-    if (this->HoldsLeft()) return Maybe<U>(fn(value()));
-    return Maybe<U>(u);
-  }
-
-  // >>=: Function (T -> Maybe<U>) -> Maybe<U>
-  template <typename Function, typename M = typename std::result_of<Function(T)>::type>
-  auto operator>>=(Function fn) const -> M {
-    if (*this) return fn((value()));
-    return Nothing;
-  }
-
-  // Returns res if there is Nothing, otherwise returns this
-  Maybe<T> operator|(const Maybe<T> &res) const { return *this ? *this : res; }
-
-  // Returns res if the is Just something, otherwise return this
-  Maybe<T> operator&(const Maybe<T> &res) const { return *this ? res : *this; }
 };
 
 template <typename T, typename U = typename std::remove_reference<T>::type>
--- src/test/include_test/result.cc
+++ src/test/include_test/result.cc
@@ -4,74 +4,88 @@
 #include <string>
 
 using namespace bolo;
+using namespace std::string_literals;
+
+int plus100(const int& a) { return a + 100; }
+
+struct Movable {
+  int value;
+  Movable(const Movable&) = delete;
+  Movable(Movable&&) = default;
+};
+
+struct Copyable {
+  int value;
+  Copyable(const Copyable&) = default;
+  Copyable(Copyable&&) = delete;
+};
 
 TEST_CASE("Result", "test") {
   int v{20};
   auto err_msg = std::string{"Unexpected Error"};
-  auto square = [](int a) { return a * a; };
+  auto square = [](const int& a) -> int { return a * a; };
 
   Result<int, std::string> r1 = Ok(v);
   Result<int, std::string> r2 = Err(err_msg);
 
   SECTION("bool") {
-    REQUIRE(static_cast<bool>(r1));
-
-    REQUIRE(static_cast<bool>(r2) == false);
+    REQUIRE(!!r1);
     REQUIRE(!r2);
   }
 
-  SECTION("value") { REQUIRE(r1.value() == v); }
-  SECTION("value_or") { REQUIRE(r2.value_or(v) == v); }
-  SECTION("error") { REQUIRE(r2.error() == err_msg); }
-  SECTION("map") {
-    auto r = r1.Map(square).Map(square);
-    REQUIRE(r.value() == v * v * v * v);
+  SECTION("value") {
+    REQUIRE(r1.value() == v);
 
-    auto r3 = r2.Map(square);
+    r1.value() += 20;
+    REQUIRE(r1.value() == v + 20);
 
-    REQUIRE(!r3);
-    REQUIRE(r3.error() == err_msg);
-    REQUIRE(r3.value_or(v) == v);
+    REQUIRE(r2.value_or(v) == v);
   }
 
-  SECTION("map_or") {
-    auto r = r2.MapOr(square, v);
-    REQUIRE(static_cast<bool>(r));
+  SECTION("error") {
+    REQUIRE(!r2);
+    REQUIRE(r2.error() == err_msg);
 
-    REQUIRE(r2.Map(square).MapOr(square, v).value() == v);
-  }
+    r2.error().append("more err msg");
 
-  SECTION(">>=") {
-    auto fn = [](int a) -> Result<double, std::string> {
-      if (a == 0)
-        return Err<std::string>("get 0");
-      else
-        return Ok(a * 3.0);
-    };
-
-    auto r = r1 >>= fn;
-    REQUIRE(static_cast<bool>(r));
-    REQUIRE(r.value() == v * 3.0);
-
-    Result<int, std::string> r3 = Ok(0);
-    auto r4 = r3 >>= fn;
-    REQUIRE(!r4);
-    REQUIRE(r4.error() == "get 0");
+    REQUIRE(r2.error() == err_msg.append("more err msg"));
   }
 
   SECTION("|") {
-    REQUIRE((r1 | Ok(2)).value() == r1.value());
-    REQUIRE((r1 | Err<std::string>("")).value() == r1.value());
+    auto r3 = r1 | plus100 | square;
+    REQUIRE(!!r3);
+    REQUIRE(r3.value() == square(plus100(r1.value())));
 
-    REQUIRE((r2 | Ok(2)).value() == 2);
-    REQUIRE((r2 | Err<std::string>("")).value_or(0) == 0);
+    auto r4 = r1 | square | plus100;
+    REQUIRE(!!r4);
+    REQUIRE(r4.value() == plus100(square(r1.value())));
   }
-  SECTION("&") {
-    REQUIRE(!(r2 & Ok(2)));
-    REQUIRE((r2 & Err<std::string>("")).error() == err_msg);
 
-    REQUIRE((r1 & Ok(2)).value() == 2);
-    REQUIRE((r1 & Err<std::string>("")).value_or(0) == 0);
+  SECTION("and or") {
+    auto r3 = r2.Or(Ok(100)) | plus100 | [](auto a) { return a * 100; };
+    REQUIRE(!!r3);
+    REQUIRE(r3.value() == plus100(r2.value_or(100)) * 100);
+
+    auto r4 = r1.And(Err("errors"s)) | plus100 | [](auto a) { return a * 100; };
+    REQUIRE(!r4);
+    REQUIRE(r4.error() == "errors"s);
+  }
+
+  SECTION(">>") {
+    auto _3div = [](int a) -> Result<int, std::string> {
+      if (a == 0) return Err("div by 0"s);
+      return Ok(3 / a);
+    };
+
+    auto r3 = Result<int, std::string>(Ok(1)) >> _3div >>
+              [](auto a) -> Result<int, std::string> { return Ok(a + 100); };
+    REQUIRE(!!r3);
+    REQUIRE(r3.value() == (_3div(1) | [](auto a) { return a + 100; }).value());
+
+    auto r4 = Result<int, std::string>(Ok(0)) >> _3div >>
+              [](auto a) -> Result<int, std::string> { return Ok(a + 100); };
+    REQUIRE(!r4);
+    REQUIRE(r4.error() == "div by 0"s);
   }
 
   SECTION("==") {
@@ -92,33 +106,19 @@ TEST_CASE("Maybe", "test") {
     REQUIRE(m2.value_or(2) == 2);
   }
 
-  SECTION("map map_or") {
-    REQUIRE(m1.Map([](int a) { return a + 100; }).value() == 102);
-    REQUIRE(m2.Map([](int a) { return a + 100; }) == Nothing);
+  SECTION("|") {}
 
-    REQUIRE(m2.MapOr([](int a) { return a + 100; }, 100) == Just(100));
-    REQUIRE(m1.MapOr([](int a) { return a + 100; }, 100) == Just(102));
-  }
-
-  SECTION(">>=") {
+  SECTION(">>") {
     auto div = [](int a) -> Maybe<int> {
       if (a == 0) return Nothing;
       return Just(a + 100);
     };
 
-    REQUIRE((m1 >>= div) == Just(102));
-    REQUIRE((Just(0) >>= div) == Nothing);
+    REQUIRE((m1 >> div) == Just(102));
   }
 
-  SECTION("|&") {
-    REQUIRE((Just(20) | Nothing) == Just(20));
-    REQUIRE((Just(20) | Just(30)) == Just(20));
-    REQUIRE((Maybe<int>(Nothing) | Just(30)) == Just(30));
-    REQUIRE((Maybe<int>(Nothing) | Nothing) == Nothing);
-
-    REQUIRE((Just(20) & Just(30)) == Just(30));
-    REQUIRE((Maybe<int>(Nothing) & Just(30)) == Nothing);
-    REQUIRE((Just(30) & Nothing) == Nothing);
-    REQUIRE((Maybe<int>(Nothing) & Nothing) == Nothing);
+  SECTION("==") {
+    REQUIRE(m1 == Just(2));
+    REQUIRE(m2 == Nothing);
   }
 }
